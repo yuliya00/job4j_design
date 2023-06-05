f7cd5084ac3ed8d648ec8765844a2b46f983980a
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2023-06-05 18:44:05 +0300

    1. Что такое итератор. [#4951  [#4951]]

---
+++ src/main/java/ru/job4j/iterator/BackwardArrayIt.java
@@ -0,0 +1,26 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+
+public class BackwardArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public BackwardArrayIt(int[] data) {
+        this.data = data;
+        point = data.length - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point >= 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point--];
+    }
+}
---
+++ src/test/java/ru/job4j/iterator/BackwardArrayItTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.iterator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BackwardArrayItTest {
+    @Test
+    void whenMultiCallHasNextThenTrue() {
+        BackwardArrayIt iterator = new BackwardArrayIt(
+                new int[] {1, 2, 3, 4}
+        );
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(4);
+        assertThat(iterator.next()).isEqualTo(3);
+        assertThat(iterator.hasNext()).isTrue();
+    }
+
+    @Test
+    void whenMultiCallHasNextThenNext() {
+        BackwardArrayIt iterator = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(3);
+
+    }
+
+    @Test
+    void whenReadSequence() {
+        BackwardArrayIt iterator = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertThat(iterator.next()).isEqualTo(3);
+        assertThat(iterator.next()).isEqualTo(2);
+        assertThat(iterator.next()).isEqualTo(1);
+        assertThat(iterator.hasNext()).isFalse();
+    }
+
+    @Test
+    void whenNextFromEmpty() {
+        BackwardArrayIt iterator = new BackwardArrayIt(
+                new int[] {}
+        );
+        assertThatThrownBy(iterator::next)
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}
