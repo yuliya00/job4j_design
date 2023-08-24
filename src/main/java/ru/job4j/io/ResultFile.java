7becfccdb890b066c332e2d8fa791ff100db4b1d
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2023-08-24 19:39:04 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 1; i < 2; i++) {
+                for (int j = 1; j < 11; j++) {
+                    String line = i + "*" + j + "=" + i * j;
+                    out.write(line.getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
