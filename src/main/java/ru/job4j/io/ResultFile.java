51fb8d7e05d6faac824992e3712c2ae0f1259ee1
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2023-08-29 18:12:29 +0300

    5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,16 +1,19 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(
+                new FileOutputStream("data/dataresult.txt")))) {
+            out.println("Hello, world!");
             for (int i = 1; i < 2; i++) {
                 for (int j = 1; j < 11; j++) {
                     String line = i + "*" + j + "=" + i * j;
-                    out.write(line.getBytes());
-                    out.write(System.lineSeparator().getBytes());
+                    out.println(line);
                 }
             }
         } catch (IOException e) {
